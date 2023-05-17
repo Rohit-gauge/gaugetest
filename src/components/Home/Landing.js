7f36ee76addf8d9b6392a3270c4416041574bda5
Rohit Bharti <71224966+Smarty6452@users.noreply.github.com>
2023-05-18 03:24:43 +0530

    update

--- src/components/Home/Landing.js
+++ src/components/Home/Landing.js
@@ -1,6 +1,13 @@
-import React from "react";
+import React, {useCallback} from "react";
 import "../../assets/css/homepage.css";
 const Landing = () => {
+  const scrollToNextSection = useCallback(() => {
+    const nextSection = document.querySelector("#next-section");
+    if (nextSection) {
+      nextSection.scrollIntoView({ behavior: "smooth" });
+    }
+  }, []);
+
   return (
     <section className="landing-page">
       <div className="xl:container mx-auto  lg:pt-16 pt-10">
@@ -8,8 +15,22 @@ const Landing = () => {
           <div className="lg:mx-auto flex flex-row px-5  pb-24 text-center ">
             <div className="grid grid-cols-1 lg:grid-cols-2 gap-6 lg:gap-4">
               <div className="lg:w-full w-full flex justify-center">
-                <img src="https://ik.imagekit.io/aq3ybtarw/landing/mobile-landing.webp?updatedAt=1680626119244" alt="hero-img"  fetchpriority="high" className="my-custom-class" />
+                <img
+                  src="https://ik.imagekit.io/aq3ybtarw/landing/mobile-landing.webp?updatedAt=1680626119244"
+                  alt="hero-img"
+                  fetchpriority="high"
+                  className="my-custom-class"
+                  srcSet="
+                    https://ik.imagekit.io/aq3ybtarw/landing/mobile-landing.webp?updatedAt=1680626119244&h=400 400w,
+                    https://ik.imagekit.io/aq3ybtarw/landing/mobile-landing.webp?updatedAt=1680626119244&h=800 800w,
+                    https://ik.imagekit.io/aq3ybtarw/landing/mobile-landing.webp?updatedAt=1680626119244&h=1200 1200w
+                  "
+                  sizes="(max-width: 300px) 400px,
+                         (max-width: 900px) 800px,
+                         1200px"
+                />
               </div>
+
               <div className="lg:col-span-1">
                 <div className="landing-widget-container mx-auto    lg:py-4 ">
                   <h1 className="rt-textappear-line70 text-center rt-textappear-line ml1 rt_gradient mb-5 xl:mb-2 sm:mt-10">
@@ -29,7 +50,10 @@ const Landing = () => {
                 </p>
 
                 <div className="flex flex-col justify-center items-center mx-auto relative ">
-                  <button className="inline-flex text-white focus:outline-none  border-1 rounded-full bg-blue-500 hover:bg-blue-600 py-4 lg:py-5 px-12 mb-7 text-base font-semibold">
+                  <button
+                    onClick={scrollToNextSection}
+                    className="inline-flex text-white focus:outline-none  border-1 rounded-full bg-blue-500 hover:bg-blue-600 py-4 lg:py-5 px-12 mb-7 text-base font-semibold"
+                  >
                     Take a product Tour
                   </button>
                   <p className="leading-relaxed text-center  font-medium text-base    mt-4 customer-para-two ">
