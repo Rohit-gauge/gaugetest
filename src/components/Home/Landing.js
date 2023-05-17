63f439a7ce6b6a0b75258d518b2cfde3d8004bab
Rohit Bharti <71224966+Smarty6452@users.noreply.github.com>
2023-05-18 03:00:32 +0530

    push

--- src/components/Home/Landing.js
+++ src/components/Home/Landing.js
@@ -1,38 +1,54 @@
 import React from "react";
 import "../../assets/css/homepage.css";
+
 const Landing = () => {
   return (
     <section className="landing-page">
-      <div className="xl:container mx-auto  lg:pt-16 pt-10">
+      <div className="xl:container mx-auto lg:pt-16 pt-10">
         <div className="text-gray-600 body-font">
-          <div className="lg:mx-auto flex flex-row px-5  pb-24 text-center ">
+          <div className="lg:mx-auto flex flex-row px-5 pb-24 text-center">
             <div className="grid grid-cols-1 lg:grid-cols-2 gap-6 lg:gap-4">
               <div className="lg:w-full w-full flex justify-center">
-                <img src="https://ik.imagekit.io/aq3ybtarw/landing/mobile-landing.webp?updatedAt=1680626119244" alt="hero-img" />
+                <picture>
+                  <source
+                    srcSet="https://ik.imagekit.io/aq3ybtarw/landing/mobile-landing.webp?updatedAt=1680626119244"
+                    type="image/webp"
+                  />
+                  <source
+                    srcSet="https://ik.imagekit.io/aq3ybtarw/landing/mobile-landing.jpg?updatedAt=1680626119244"
+                    type="image/jpeg"
+                  />
+                  <img
+                    src="https://ik.imagekit.io/aq3ybtarw/landing/mobile-landing.jpg?updatedAt=1680626119244"
+                    alt="hero-img"
+                    className="my-custom-class"
+                    loading="lazy"
+                  />
+                </picture>
               </div>
               <div className="lg:col-span-1">
-                <div className="landing-widget-container mx-auto    lg:py-4 ">
+                <div className="landing-widget-container mx-auto lg:py-4">
                   <h1 className="rt-textappear-line70 text-center rt-textappear-line ml1 rt_gradient mb-5 xl:mb-2 sm:mt-10">
                     Gauge Water Purifier{" "}
                     <span className="rt-heading-div">
                       <span className="text-wrapper">
                         <span className="letters">puts an end</span>{" "}
-                        <span className="line line2" />{" "}
+                        <span className="line line2" />
                       </span>{" "}
                     </span>{" "}
                     <span className="after-text">to RO service pain</span>
                   </h1>
                 </div>
-                <p className="mb-12 px-4 md:px-16 lg:px-0 lg:mx-3 leading-relaxed text-center  font-medium text-base md:text-lg lg:text-xl max-w-2xl customer-para-one">
+                <p className="mb-12 px-4 md:px-16 lg:px-0 lg:mx-3 leading-relaxed text-center font-medium text-base md:text-lg lg:text-xl max-w-2xl customer-para-one">
                   Fully automatic water purifier provides the highest water
                   savings while ensuring a seamless service experience.
                 </p>
 
-                <div className="flex flex-col justify-center items-center mx-auto relative ">
-                  <button className="inline-flex text-white focus:outline-none  border-1 rounded-full bg-blue-500 hover:bg-blue-600 py-4 lg:py-5 px-12 mb-7 text-base font-semibold">
+                <div className="flex flex-col justify-center items-center mx-auto relative">
+                  <button className="inline-flex text-white focus:outline-none border-1 rounded-full bg-blue-500 hover:bg-blue-600 py-4 lg:py-5 px-12 mb-7 text-base font-semibold">
                     Take a product Tour
                   </button>
-                  <p className="leading-relaxed text-center  font-medium text-base    mt-4 customer-para-two ">
+                  <p className="leading-relaxed text-center font-medium text-base mt-4 customer-para-two">
                     Experience Convenience Like Never Before With Our Smart
                     Water Purifier
                   </p>
