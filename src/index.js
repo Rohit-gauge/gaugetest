75b826bc9e2e24bab6f1f19d8052a07b1a906ffa
Rohit Bharti <71224966+Smarty6452@users.noreply.github.com>
2023-05-18 00:46:54 +0530

    Update

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
 import criticalCSS from "./index.css";
+import App from "./App";
 
 function loadCriticalCSS() {
   const styleTag = document.createElement("style");
